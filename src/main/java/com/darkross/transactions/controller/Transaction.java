1253341843462d246db4739ede69a4d6e2c1f66e
DARKROSS <andresarcangel00@gmail.com>
2025-08-15 15:54:04 -0500

    correcion

--- src/main/java/com/darkross/transactions/controller/Transaction.java
+++ src/main/java/com/darkross/transactions/controller/Transaction.java
@@ -1,11 +1,13 @@
 package com.darkross.transactions.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/transactions")
 public class Transaction {
+    @GetMapping
     public String getTransactions() {
         return "List of transactions";
     }
